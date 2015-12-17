ca99810378879e978d75cea54c9350739f01e117
Guillermo Kardolus <kardolus@gmail.com>
2015-12-17 11:13:19 -0500

    Updated test method after tests with a valid API key.

--- test/us/kardol/cta/TrainTrackerTest.java
+++ test/us/kardol/cta/TrainTrackerTest.java
@@ -2,6 +2,7 @@ package us.kardol.cta;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -9,60 +10,66 @@ import static org.junit.Assert.*;
  */
 public class TrainTrackerTest {
 
+    TrainTracker tt;
+    
+    @Before
+    public void setUp() {
+        tt = new TrainTracker();
+        tt.setKey("Invalid_key"); // From CTA docs
+    }
     /**
      * Test of getArrivals method, of class TrainTracker.
      */
     @Test
     public void testGetArrivals() {
-        TrainTracker tt = new TrainTracker();
-        tt.setKey("a8456dcbhf8475683cf7818bca81"); // From CTA docs
         tt.setMapId(40380);
         tt.setMaxResults(5);
   
         String result = tt.getArrivals();
         
         System.out.print("Test 1...");
-        assert(result.contains("Invalid API key")) : "Example key should not be valid";
+        if(!result.contains("<errCd>101</errCd>")){
+            assert(result.contains("<destNm>")) : "No connection could be made";
+        }
         System.out.println(" ok");
         
         System.out.print("Test 2...");
-        assertEquals("http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=a8456dcbhf8475683cf7818bca81&mapid=40380&max=5", 
+        assertEquals("http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=" + tt.getKey() + "&mapid=40380&max=5", 
                 tt.getStringUrl());
         System.out.println(" ok");
     }
     
     @Test
     public void testFollowThisTrain() {
-        TrainTracker tt = new TrainTracker();
-        tt.setKey("a8456dcbhf8475683cf7818bca81"); // From CTA docs
         tt.setRunNumber(426);
   
         String result = tt.followThisTrain();
  
         System.out.print("Test 3...");
-        assert(result.contains("Invalid API key")) : "Example key should not be valid";
+        if(!result.contains("<errCd>101</errCd>")){
+            assert(result.contains("No trains with runnumber 426 were found.")) : "No connection could be made";
+        }
         System.out.println(" ok");
         
         System.out.print("Test 4...");
-        assertEquals("http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=a8456dcbhf8475683cf7818bca81&runnumber=426", 
+        assertEquals("http://lapi.transitchicago.com/api/1.0/ttfollow.aspx?key=" + tt.getKey() + "&runnumber=426", 
                 tt.getStringUrl());
         System.out.println(" ok");
     }
  
     @Test
     public void testGetLocations() {
-        TrainTracker tt = new TrainTracker();
-        tt.setKey("a8456dcbhf8475683cf7818bca81"); // From CTA docs
         tt.setRouteCode("red");
-  
         String result = tt.getLocations();
  
         System.out.print("Test 5...");
-        assert(result.contains("Invalid API key")) : "Example key should not be valid";
+        if(!result.contains("<errCd>101</errCd>")){
+            assert(result.contains("<destNm>")) : "No connection could be made";
+        }
         System.out.println(" ok");
         
         System.out.print("Test 6...");
-        assertEquals("http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=a8456dcbhf8475683cf7818bca81&rt=red", 
+        assertEquals("http://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=" + tt.getKey() + "&rt=red", 
                 tt.getStringUrl());
         System.out.println(" ok");
     }
